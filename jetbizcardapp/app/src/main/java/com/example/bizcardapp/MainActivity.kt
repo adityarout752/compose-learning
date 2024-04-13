8be28ec880e0dcf7bca34ed89c2b5e59d9edb320
Aditya <adityaroutgood@gmail.com>
2024-04-13 22:11:19 +0530

    biz card ui completed

--- jetbizcardapp/app/src/main/java/com/example/bizcardapp/MainActivity.kt
+++ jetbizcardapp/app/src/main/java/com/example/bizcardapp/MainActivity.kt
@@ -3,13 +3,43 @@ package com.example.bizcardapp
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.BorderStroke
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.foundation.shape.CornerSize
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Button
+import androidx.compose.material3.Card
+import androidx.compose.material3.Divider
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.RectangleShape
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.example.bizcardapp.ui.theme.BizcardappTheme
 
 class MainActivity : ComponentActivity() {
@@ -22,7 +52,7 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    Greeting("Android")
+                    Bizcard()
                 }
             }
         }
@@ -30,17 +60,141 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
+fun Bizcard() {
+    val buttonClickedState = remember {
+        mutableStateOf(false)
+    }
+    Surface(
+        modifier = Modifier
+            .fillMaxWidth()
+            .fillMaxHeight()
+    ) {
+        Card(
+            modifier = Modifier
+                .fillMaxWidth()
+                .fillMaxHeight()
+                .padding(12.dp),
+            shape = RoundedCornerShape(corner = CornerSize(15.dp)),
+        ) {
+            Column(
+                modifier = Modifier.fillMaxSize(),
+                verticalArrangement = Arrangement.Top,
+                horizontalAlignment = Alignment.CenterHorizontally
+            ) {
+
+                profileImage()
+                Divider(thickness = 2.dp)
+                createInfo()
+                Button(onClick = { buttonClickedState.value = !buttonClickedState.value }) {
+                    Text(text = "Portfolio", style = MaterialTheme.typography.bodyMedium)
+                    }
+                if (buttonClickedState.value) {
+                    Content()
+                } else {
+                    Box() {
+
+                    }
+                }
+            }
+
+        }
+    }
+}
+
+@Composable
+private fun Content() {
+    Box(
+        modifier = Modifier
+            .fillMaxWidth()
+            .fillMaxHeight()
+            .padding(5.dp)
+    ) {
+        Surface(
+            modifier = Modifier
+                .fillMaxWidth()
+                .fillMaxHeight()
+                .padding(3.dp),
+            shape = RoundedCornerShape(corner = CornerSize(6.dp)), border = BorderStroke(
+                4.dp,
+                Color.Gray
+            ), color = Color.Transparent
+        ) {
+            Portfolio(data = listOf<String>("Android", "Java", "Kotlin", "Jetpack Compose", "Mvvm"))
+        }
+    }
+}
+
+@Composable
+fun Portfolio(data: List<String>) {
+    LazyColumn {
+        items(data) { item ->
+            Card(
+                modifier = Modifier
+                    .padding(top=13.dp, start = 13.dp,end=13.dp)
+                    .fillMaxWidth(), shape = RectangleShape
+            ) {
+                Row(
+                    modifier = Modifier.fillMaxWidth()
+                        .background(MaterialTheme.colorScheme.surface)
+                        .padding(10.dp)
+                ) {
+                    profileImage(modifier = Modifier.size(100.dp))
+                    Column(
+                        modifier = Modifier
+                            .padding(7.dp)
+                            .align(alignment = Alignment.CenterVertically)
+                    ) {
+                             Text(text = item, fontWeight = FontWeight.Bold)
+                        Text(text = "Good project", style = MaterialTheme.typography. bodyMedium)
+
+                    }
+                }
+            }
+        }
+    }
+}
+
+@Composable
+private fun createInfo() {
+    Column(modifier = Modifier.padding(5.dp)) {
+        Text(
+            text = "Aditya Rout",
+            style = MaterialTheme.typography.headlineMedium,
+            color = MaterialTheme.colorScheme.primary
+        )
+        Text(
+            text = "Compose Programmer",
+            modifier = Modifier.padding(3.dp)
+        )
+        Text(
+            text = "email id of Aditya Rout",
+            modifier = Modifier.padding(3.dp)
+        )
+
+    }
+}
+
+@Composable
+private fun profileImage(modifier: Modifier = Modifier) {
+    Surface(
         modifier = modifier
-    )
+            .size(150.dp)
+            .padding(5.dp), shape = CircleShape,
+        border = BorderStroke(0.5.dp, Color.LightGray), shadowElevation = 4.dp
+    ) {
+        Image(
+            painter = painterResource(id = R.drawable.baseline_person_24),
+            contentDescription = "Image",
+            modifier = modifier.size(135.dp),
+            contentScale = ContentScale.Crop
+        )
+    }
 }
 
 @Preview(showBackground = true)
 @Composable
 fun GreetingPreview() {
     BizcardappTheme {
-        Greeting("Android")
+        Bizcard()
     }
 }
